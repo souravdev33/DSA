b25ef59cfb25a0bbb0e2ce464ca25ba730b481b9
Sourav Dev <130246603+souravdev33@users.noreply.github.com>
2024-05-13 13:34:59 +0600

    Add files via upload

---
+++ Sorting/Bubblesort.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+void Bubble_sort(int arr[],int n){
+    for(int i=n-1;i>=1;i--){
+        for(int j=0;j<=i-1;j++){
+            if(arr[j]>arr[j+1]){
+                int temp=arr[j];;
+                arr[j]=arr[j+1];
+                arr[j+1]=temp;
+            }
+        }
+    }
+}
+int main(){
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    Bubble_sort(arr,n);
+    cout<<"After sorting : ";
+    for(int i=0;i<n;i++){
+       cout<<arr[i]<<" ";
+    }
+return 0;
+}
